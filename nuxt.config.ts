92778671e5764d8ee259cd2840b69bc23f2fa03d
sibelgoktug <sibelgoktuug@gmail.com>
2024-01-05 18:10:01 +0300

    header completed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,26 @@ export default defineNuxtConfig({
     }
   },
   css : [
-    '~/assets/style.scss'
+    '~/public/css/bootstrap.css',
+    '~/public/css/bootstrap-grid.css',
+    '~/assets/style.scss',
+
+  ],
+  app: {
+    head: {
+      script: [
+        { src: "~/js/bootstrap.js" },
+        { src: "~/js/bootstrap.min.js" },
+        { src: "/js/bootstrap.bundle.min.js" },
+
+      ],
+    },
+  },
+  components: [
+    {
+      path: "~/components", // will get any components nested in let's say /components/test too
+      pathPrefix: false,
+    },
   ],
 })
 
