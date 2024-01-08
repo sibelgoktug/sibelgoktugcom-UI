4ec7cb2596a6b1c31a4e045a7e250e95e96fd8dc
sibelgoktug <sibelgoktuug@gmail.com>
2024-01-08 14:05:20 +0300

    top content area completed

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <Header />
+  <TopContent />
 </template>
 
 <script>
 import Header from '~/components/layout/header.vue';
-
+import TopContent from '~/components/home/TopContent.vue';
 export default {
   name: "HomePage",
   components: {
     Header,
+    TopContent
   },
 };
 </script>
